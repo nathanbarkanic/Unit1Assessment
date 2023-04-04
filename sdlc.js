fc906a01c1529fdc2fe96a3a948e1ddd2377185c
nathan <nathanmedia858@gmail.com>
2023-04-04 15:41:28 -0700

    update

--- sdlc.js
+++ sdlc.js
@@ -21,6 +21,6 @@
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added."; 
 const AnalysisofRequirements = "This step is done to clearly define and document the product requirments and get them approved from the customer or the market analysis.";
 const Design = "The Design step is the reference for product architects to come out with the best architecture for the product to be developed.";
+const Implementation = "In this step you are coding the project according to the requirements and design.";
 const TestingandIntegration = "This step is where the product defects are reported, tracked, fixed and retested until the product reaches it's goals and standards";
-const Implementation = "In this step you are now deploying your product into the market.";
 const Maintain = "This step contiues as your product in working in the market to continue to imporove upon customer feeback and fix and track any potential future defects."
